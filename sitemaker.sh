df57bf67c1e145d953964b9fafdcb1f83947dfd0
w7tree <w7tree@gmail.com>
2013-05-23 17:37:32 +0900

    [起動用シェルスクリプトを追加]

---
+++ sitemaker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+TEMP=$_
+DIR=`echo $TEMP | sed -e s/sitemaker.sh$//`
+
+cp -r $DIR/* ./
+rm ./sitemaker.sh
+
+echo "Do you create wordpress theme?(y/n)"
+read answer
+
+if [ "$answer" = y ]
+then
+  rm -rf ./_include
+else
+  rm ./functions.php
+fi
+
